89f13a736bcb2121115c078d11034dd7ed142d89
James <89645770+james-netbiz@users.noreply.github.com>
2024-03-07 10:50:08 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,67 @@
+<section>
+	<h1>Netbiz Holding Page</h1>
+	<h2>This website is launching real soon! 🤘🏻</h2>
+	<h3><a href="https://netbizgroup.co.uk">Visit us:</a></h3>
+</section>
+
+<footer>Website currently under development by <a href="https://netbizgroup.co.uk" title="Staffordshire Website Developers">Netbiz Group</a></footer>
+
+<style>
+body {
+	font-family: 'Open Sans';
+	background: url(https://www.netbizgroup.co.uk/wp-content/uploads/2022/04/Careers_2.jpg);
+	background-size: cover;
+	background-repeat: no-repeat;
+	color: #FFF;
+}
+
+section {
+	display: block;
+	width: 100%;
+	max-width: 420px;
+	position: fixed;
+	top: 50px;
+	left: 0%;
+	padding: 0px;
+	text-transform: uppercase;
+	text-align: left;
+	text-shadow: 0px 0px 62px #000, 0px 2px 6px #000;
+	transform: skewY(-10deg);
+}
+
+h1, h3 {
+	margin: 0px;
+	font-size: 1em;
+	display: inline-block;
+	padding: .5em .5em .5em 50px;
+	text-shadow: 0px 0px 62px #000, 0px 2px 2px #000;
+	background: rgba(0,0,0,.75);
+}
+
+h2 {
+	font-weight: 900;
+	font-size: 2.6em;
+	display: inline-block;
+	background: rgba(0,0,0,.75);
+	color: #FFF;
+	margin: .1em 0px;
+	padding: .5em .5em .5em 50px;
+}
+
+footer {
+	display: block;
+	font-size: 12px;
+	background: rgba(0,0,0,.75);
+	position: fixed;
+	right: 0px;
+	bottom: 50px;
+	padding: 1em;
+	transform: skewY(-10deg);
+	text-align: center;
+}
+
+footer a {
+	font-weight: 700;
+	text-decoration: none;
+	color: #FFF;
+}</style>
